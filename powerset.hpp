2066d15ad8095a2b7deb3ccf711544ec4d7f33a9
oravital7 <oravital7@gmail.com>
2019-05-09 10:47:27 +0300

    support powerset

---
+++ powerset.hpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+namespace itertools
+{
+template <typename T>
+class Itpowerset
+{
+private:
+    T a;
+
+public:
+    Itpowerset(T a) : a(a) {}
+
+    class iterator
+    {
+        std::pair<T, uint> it;
+
+    public:
+        iterator(std::pair<T, uint> it) : it(it) {}
+
+        std::pair<T, uint> &operator*()
+        {
+            return it;
+        }
+
+        bool operator==(const iterator &other) const
+        {
+            return it.second == other.it.second;
+        }
+
+        bool operator!=(const iterator &other) const
+        {
+            return it.second != other.it.second;
+        }
+
+        iterator &operator=(const iterator &other)
+        {
+            it.first = other.it.first;
+            it.second = other.it.second;
+            return *this;
+        }
+
+        // ++i;
+        iterator &operator++()
+        {
+            it.second++;
+            return *this;
+        }
+
+        // i++;
+        const iterator operator++(int)
+        {
+            iterator tmp = *this;
+            it.second++;
+            return tmp;
+        }
+    };
+
+public:
+    auto begin()
+    {
+        return iterator{std::pair<T, uint>(a, 0)};
+    }
+
+    auto end()
+    {
+        int i = 1;
+        auto it = a.begin();
+        while (it != a.end())
+        {
+            i = i << 1;
+            it++;
+        }
+        return iterator{std::pair<T, uint>(a, i)};
+    }
+};
+
+template <typename T>
+auto powerset(T a)
+{
+    return Itpowerset<T>(a);
+}
+
+} // namespace itertools
+
+template <typename T1>
+std::ostream &operator<<(std::ostream &os, std::pair<T1, uint> &a)
+{
+    os << "{";
+    auto it = a.first.begin();
+    auto end = a.first.end();
+    int i = 1;
+    bool first = true;
+    while (it != end)
+    {
+        if (i & a.second)
+        {
+            if (!first)
+            {
+                os << ",";
+            }
+            os << *it;
+            first = false;
+        }
+        it++;
+        i = i << 1;
+    }
+    os << "}";
+    return os;
+}
---
+++ product.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+namespace itertools
+{
+
+template <typename T1, typename T2>
+class Itproduct
+{
+
+private:
+    std::pair<T1, T2> a;
+    std::pair<T1, T2> b;
+
+public:
+    Itproduct(std::pair<T1, T2> a, std::pair<T1, T2> b) : a(a), b(b) {}
+
+    class iterator
+    {
+    private:
+        std::pair<T1, T2> it;
+        std::pair<T2, T2> b;
+
+    public:
+        iterator(std::pair<T1, T2> it, std::pair<T2, T2> b) : it(it), b(b) {}
+
+        std::pair<T1, T2> &operator*()
+        {
+            return it;
+        }
+
+        bool operator==(const iterator &other) const
+        {
+            return it.first == other.it.first;
+        }
+
+        bool operator!=(const iterator &other) const
+        {
+            return it.first != other.it.first;
+        }
+
+        iterator &operator=(const iterator &other)
+        {
+            this->it.first = other.it.first;
+            this->it.second = other.it.second;
+            this->b.first = other.b.first;
+            this->b.second = other.b.second;
+            return *this;
+        }
+
+        // ++i;
+        iterator &operator++()
+        {
+            it.second++;
+            if (it.second == b.second)
+            {
+                it.first++;
+                it.second = b.first;
+            }
+            return *this;
+        }
+
+        // i++;
+        const iterator operator++(int)
+        {
+            iterator tmp = *this;
+            it.second++;
+            if (it.second == b.second)
+            {
+                it.first++;
+                it.second = b.first;
+            }
+            return tmp;
+        }
+    };
+
+public:
+    auto begin()
+    {
+        return iterator{a, pair<T2, T2>(a.second, b.second)};
+    }
+
+    auto end()
+    {
+        return iterator{pair<T1, T2>(b.first, b.second), pair<T2, T2>(a.second, b.second)};
+    }
+};
+
+template <typename T1, typename T2>
+auto product(T1 a, T2 b)
+{
+    std::pair<decltype(a.begin()), decltype(b.begin())> aPair(a.begin(), b.begin());
+    std::pair<decltype(a.end()), decltype(b.end())> bPair(a.end(), b.end());
+
+    return Itproduct<decltype(a.begin()), decltype(b.begin())>(aPair, bPair);
+}
+
+} // namespace itertools
